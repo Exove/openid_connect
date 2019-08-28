8276d8f8a4357408bafe19d57129205f6dab796a
Patrik Hirvinen <patrik.hirvinen@exove.fi>
2019-08-28 13:16:05 +0300

    2961752: Replace drupal_set_message with MessengerTrait and use it properly.

--- src/Form/OpenIDConnectAccountsForm.php
+++ src/Form/OpenIDConnectAccountsForm.php
@@ -107,8 +107,7 @@ class OpenIDConnectAccountsForm extends FormBase implements ContainerInjectionIn
       $container->get('openid_connect.authmap'),
       $container->get('openid_connect.claims'),
       $container->get('plugin.manager.openid_connect_client.processor'),
-      $container->get('config.factory'),
-      $container->get('messenger')
+      $container->get('config.factory')
     );
   }
 
@@ -193,12 +192,12 @@ class OpenIDConnectAccountsForm extends FormBase implements ContainerInjectionIn
     if ($op === 'disconnect') {
       $this->authmap->deleteAssociation($form_state->get('account')->id(), $client_name);
       $client = $this->pluginManager->getDefinition($client_name);
-      drupal_set_message($this->t('Account successfully disconnected from @client.', ['@client' => $client['label']]));
+      $this->messenger()->addStatus($this->t('Account successfully disconnected from @client.', ['@client' => $client['label']]));
       return;
     }
 
     if ($this->currentUser->id() !== $form_state->get('account')->id()) {
-      drupal_set_message($this->t("You cannot connect another user's account."), 'error');
+      $this->messenger()->addError($this->t("You cannot connect another user's account."));
       return;
     }
 
