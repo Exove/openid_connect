514a80b7f48e9b16eda6f8ee26bed968ad78572d
Patrik Hirvinen <patrik.hirvinen@exove.fi>
2019-09-23 10:50:21 +0300

    3076619, 3082373: Add configuration to Stateful client base.

--- src/Plugin/OpenIDConnectStatefulClientBase.php
+++ src/Plugin/OpenIDConnectStatefulClientBase.php
@@ -5,6 +5,8 @@ namespace Drupal\openid_connect\Plugin;
 use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\openid_connect\OpenIDConnectAuthmap;
 use Drupal\user\UserInterface;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Base class for stateful OpenID Connect client plugins.
@@ -118,6 +120,362 @@ abstract class OpenIDConnectStatefulClientBase extends OpenIDConnectClientBase i
    * @see hook_openid_connect_userinfo_alter()
    */
   protected $userInfo;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'client_id' => '',
+      'client_secret' => '',
+      'issuer_identifier' => '',
+      'use_discovery' => TRUE,
+      'discovery_uri' => '',
+      'authorization_endpoint' => '',
+      'token_endpoint' => '',
+      'userinfo_endpoint' => '',
+      'jwks_uri' => '',
+      'use_request_object' => TRUE,
+      'encrypt_authorization_request' => TRUE,
+      'request_object_encryption_alg_values_supported_whitelist' => '',
+      'request_object_encryption_alg_values_supported_blacklist' => '',
+      'request_object_encryption_enc_values_supported_whitelist' => '',
+      'request_object_encryption_enc_values_supported_blacklist' => '',
+      'require_id_token_encryption' => TRUE,
+      'id_token_encryption_alg_values_supported_whitelist' => '',
+      'id_token_encryption_alg_values_supported_blacklist' => '',
+      'id_token_encryption_enc_values_supported_whitelist' => '',
+      'id_token_encryption_enc_values_supported_blacklist' => '',
+      'id_token_signing_alg_values_supported_whitelist' => '',
+      'id_token_signing_alg_values_supported_blacklist' => '',
+      'use_userinfo_endpoint' => TRUE,
+      'require_userinfo_encryption' => TRUE,
+      'userinfo_encryption_alg_values_supported_whitelist' => '',
+      'userinfo_encryption_alg_values_supported_blacklist' => '',
+      'userinfo_encryption_enc_values_supported_whitelist' => '',
+      'userinfo_encryption_enc_values_supported_blacklist' => '',
+      'require_userinfo_signature' => TRUE,
+      'userinfo_signing_alg_values_supported_whitelist' => '',
+      'userinfo_signing_alg_values_supported_blacklist' => '',
+      'client_key_id' => '',
+      'client_key' => '',
+      'show_advanced_cryptography_settings' => FALSE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $redirect_url = URL::fromRoute(
+      'openid_connect.redirect_controller_redirect',
+      [
+        'client_name' => $this->pluginId,
+      ],
+      [
+        'absolute' => TRUE,
+      ]
+    );
+    $form['redirect_url'] = [
+      '#title' => $this->t('Redirect URL'),
+      '#description' => $this->t('Identity Provider will redirect the user to this URL after the authorization.'),
+      '#type' => 'item',
+      '#markup' => $redirect_url->toString(),
+    ];
+    $form['client_id'] = [
+      '#title' => $this->t('Client ID'),
+      '#description' => $this->t('The client ID is used to identify your service to the Identity Provider.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['client_id'],
+    ];
+    $form['client_secret'] = [
+      '#title' => $this->t('Client secret'),
+      '#description' => $this->t('The client secret is used to identify your service to the Identity Provider.'),
+      '#type' => 'textfield',
+      '#maxlength' => 1024,
+      '#default_value' => $this->configuration['client_secret'],
+    ];
+    $form['issuer_identifier'] = [
+      '#title' => $this->t('Identity Provider Issuer Identifier'),
+      '#description' => $this->t('An HTTPS URL identifying the Identity Provider.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['issuer_identifier'],
+    ];
+    $form['use_discovery'] = [
+      '#title' => $this->t('Use OpenID Connect Discovery'),
+      '#description' => $this->t('Try to determine appropriate settings directly from the Identity Provider.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['use_discovery'],
+    ];
+
+    // @todo Hide when discovery is not enabled.
+    $form['discovery_uri'] = [
+      '#title' => $this->t('OpenID Connect Discovery URL'),
+      '#description' => $this->t('If empty, use the standard URL: &lt;Issuer Identifier&gt;/.well-known/openid-configuration is used.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['discovery_uri'],
+    ];
+    // @todo Hide when discovery is enabled.
+    $form['authorization_endpoint'] = [
+      '#title' => $this->t('Authorization endpoint'),
+      '#description' => $this->t('Identity Provider URL where users are redirected to approve the login request.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['authorization_endpoint'],
+    ];
+    // @todo Hide when discovery is enabled.
+    $form['token_endpoint'] = [
+      '#title' => $this->t('Token endpoint'),
+      '#description' => $this->t('Identity Provider URL from which ID Token identifying the user will be fetched after Authorization.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['token_endpoint'],
+    ];
+    // @todo Hide when discovery is enabled.
+    // @todo require when discovery is not enabled and userinfo endpoint is required.
+    $form['userinfo_endpoint'] = [
+      '#title' => $this->t('UserInfo endpoint'),
+      '#description' => $this->t('Identity Provider URL from which additional user information may be fetched.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_endpoint'],
+    ];
+    // @todo Hide when discovery is enabled.
+    $form['jwks_uri'] = [
+      '#title' => $this->t('JSON Web Key Set URI'),
+      '#description' => $this->t('The URL from which to fetch the public encryption and signing keys used by the Identity Provider.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['jwks_uri'],
+    ];
+    // @todo Validate when discovery is enabled.
+    $form['use_request_object'] = [
+      '#title' => $this->t('Use a Request object for the Authorization Request'),
+      '#description' => $this->t('Sending the Authorization Request using a Request object may be necessary with some Identity Providers.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['use_request_object'],
+    ];
+    // @todo hide when request object is not used.
+    $form['encrypt_authorization_request'] = [
+      '#title' => $this->t('Encrypt Authorization Request'),
+      '#description' => $this->t('Some Identity Providers may require encryption of the Request object.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['encrypt_authorization_request'],
+    ];
+    $form['require_id_token_encryption'] = [
+      '#title' => $this->t('Do not accept unencrypted ID Tokens'),
+      '#description' => $this->t('OpenID Connect does not mandate ID Token encryption, but some Identity Providers may do so.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['require_id_token_encryption'],
+    ];
+    $form['use_userinfo_endpoint'] = [
+      '#title' => $this->t('Get additional Userinfo from the UserInfo endpoint'),
+      '#description' => $this->t('Some Identity Providers supply all user information in the ID Token, which makes a separate UserInfo request unnecessary. Also some Identity Providers may not support the UserInfo endpoint.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['use_userinfo_endpoint'],
+    ];
+    $form['require_userinfo_encryption'] = [
+      '#title' => $this->t('Do not acept unencrypted UserInfo responses'),
+      '#description' => $this->t('OpenID Connect does not mandate UserInfo response encryption, but some Identity Providers may do so.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['require_userinfo_encryption'],
+    ];
+
+    $form['require_userinfo_signature'] = [
+      '#title' => $this->t('Do not accept unsigned UserInfo responses'),
+      '#description' => $this->t('OpenID Connect does not mandate UserInfo response signing, but some Identity Providers may do so.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['require_userinfo_signature'],
+    ];
+
+    $form['client_key_id'] = [
+      '#title' => $this->t('Client key Key ID'),
+      '#description' => $this->t('Key ID provided by the Identity Provider for your public key.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['client_key_id'],
+    ];
+    $form['client_key'] = [
+      '#title' => $this->t('Client key'),
+      '#description' => $this->t('A JSON Web Key containing for decrypting ID Token and UserInfo responses.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['client_key'],
+    ];
+    $form['show_advanced_cryptography_settings'] = [
+      '#title' => $this->t('Show advanced cryptography settings'),
+      '#description' => $this->t('Additional settings for encryption and signature algorithms.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->configuration['show_advanced_cryptography_settings'],
+    ];
+    // Request related crypto settings.
+    // @todo hide when request object is not used.
+    $form['request_object_encryption_alg_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed request object encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['request_object_encryption_alg_values_supported_whitelist'],
+    ];
+    $form['request_object_encryption_alg_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden request object encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['request_object_encryption_alg_values_supported_blacklist'],
+    ];
+    $form['request_object_encryption_enc_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed request object encryption algorithms for content encryption ("enc")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['request_object_encryption_enc_values_supported_whitelist'],
+    ];
+    $form['request_object_encryption_enc_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden request object encryption algorithms for content encryption ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['request_object_encryption_enc_values_supported_blacklist'],
+    ];
+    // ID Token related crypto settings.
+    $form['id_token_encryption_alg_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed ID Token encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_encryption_alg_values_supported_whitelist'],
+    ];
+    $form['id_token_encryption_alg_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden ID Token encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_encryption_alg_values_supported_blacklist'],
+    ];
+    $form['id_token_encryption_enc_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed ID Token encryption algorithms for content encryption ("enc")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_encryption_enc_values_supported_whitelist'],
+    ];
+    $form['id_token_encryption_enc_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden ID Token encryption algorithms for content encryption ("enc")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_encryption_enc_values_supported_blacklist'],
+    ];
+    $form['id_token_signing_alg_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed ID Token signature algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_signing_alg_values_supported_whitelist'],
+    ];
+    $form['id_token_signing_alg_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden ID Token signature algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['id_token_signing_alg_values_supported_blacklist'],
+    ];
+    // Userinfo related crypto settings.
+    // @todo hide when Userinfo endpoint is not used.
+    $form['userinfo_encryption_alg_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed UserInfo encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_encryption_alg_values_supported_whitelist'],
+    ];
+    $form['userinfo_encryption_alg_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden UserInfo encryption algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_encryption_alg_values_supported_blacklist'],
+    ];
+    $form['userinfo_encryption_enc_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed UserInfo encryption algorithms for content encryption ("enc")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_encryption_enc_values_supported_whitelist'],
+    ];
+    $form['userinfo_encryption_enc_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden UserInfo encryption algorithms for content encryption ("enc")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_encryption_enc_values_supported_blacklist'],
+    ];
+    $form['userinfo_signing_alg_values_supported_whitelist'] = [
+      '#title' => $this->t('Allowed UserInfo signature algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of algorithms in decreasing order of preference.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_signing_alg_values_supported_whitelist'],
+    ];
+    $form['userinfo_signing_alg_values_supported_blacklist'] = [
+      '#title' => $this->t('Forbidden UserInfo signature algorithms for key management ("alg")'),
+      '#description' => $this->t('A comma separated list of forbidden algorithms.'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_signing_alg_values_supported_blacklist'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+    // Provider label as array for StringTranslationTrait::t() argument.
+    $provider = [
+      '@provider' => $this->getPluginDefinition()['label'],
+    ];
+
+    // Get plugin setting values.
+    $configuration = $form_state->getValues();
+
+    // Whether a client ID is given.
+    if (empty($configuration['client_id'])) {
+      $form_state->setErrorByName('client_id', $this->t('The client ID is missing for @provider.', $provider));
+    }
+    // Whether a client secret is given.
+    if (empty($configuration['client_secret'])) {
+      $form_state->setErrorByName('client_secret', $this->t('The client secret is missing for @provider.', $provider));
+    }
+
+    if (!empty($configuration['use_discovery'])) {
+      // For discovery, we need the issuer identifier or a discovery uri.
+      $issuer_identifier = $configuration['issuer_identifier'] ?? '';
+      $discovery_uri = $configuration['discovery_uri'] ?? '';
+      if (empty($issuer_identifier)) {
+        if (!UrlHelper::isValid($discovery_uri)) {
+          $form_state->setErrorByName('discovery_uri', $this->t('If Issuer Identifier is not given, the Discovery URL for @provider must be a valid URL when using autodiscovery.', $provider));
+        }
+      }
+      elseif (UrlHelper::isValid($issuer_identifier)) {
+        // With a valida issuer identifier, the discovery uri should be valid
+        // or empty.
+        if (!empty($discovery_uri) && !UrlHelper::isValid($discovery_uri)) {
+          $form_state->setErrorByName('discovery_uri', $this->t('When autodiscovery is used and an Issuer Identifier is given, The Discovery URL for @provider must be a valid URL or empty.', $provider));
+        }
+      }
+      else {
+        $form_state->setErrorByName('issuer_identifier', $this->t('Issuer Identifier for @provider must be a valid URL or empty.', $provider));
+      }
+    }
+    else {
+      if (empty($configuration['authorization_endpoint']) || !UrlHelper::isValid($configuration['authorization_endpoint'])) {
+        $form_state->setErrorByName('authorization_endpoint', 'When not using autodiscovery, Authorization endpoint for @provider must be a valid URL.', $provider);
+      }
+      if (empty($configuration['token_endpoint']) || !UrlHelper::isValid($configuration['token_endpoint'])) {
+        $form_state->setErrorByName('token_endpoint', 'When not using autodiscovery, Token endpoint for @provider must be a valid URL.', $provider);
+      }
+      if (empty($configuration['userinfo_endpoint']) || !UrlHelper::isValid($configuration['userinfo_endpoint'])) {
+        if (!empty($configuration['use_userinfo_endpoint'])) {
+          $form_state->setErrorByName('userinfo_endpoint', 'When not using autodiscovery, UserInfo endpoint for @provider must be a valid URL.', $provider);
+        }
+      }
+      if (empty($configuration['jwks_uri']) || !UrlHelper::isValid($configuration['jwks_uri'])) {
+        $form_state->setErrorByName('jwks_uri', 'When not using autodiscovery, the JWKS URI for @provider must be a valid URL.', $provider);
+      }
+    }
+
+    // @todo Validate use_request_object when discovery is enabled.
+    // @todo Validate client_key_id and client_key when encryption is required.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    // No need to do anything, but make the function have a body anyway
+    // so that it's callable by overriding methods.
+  }
 
   /**
    * Get the logger channel for the client plugin.
